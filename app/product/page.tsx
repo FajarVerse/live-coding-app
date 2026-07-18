3a10c3f636278302cc5a1e2031268a34a1b52ab4
FajarVerse <gifarifajarm174@gmail.com>
2026-07-18 09:52:49 +0700

    feat: creat detail product & fixing layout product page

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -2,7 +2,6 @@
 
 import type { Auth } from "@/app/login/page";
 import ContainerLayout from "@/layouts/container-layout";
-import Header from "@/layouts/header";
 import { getProduct } from "@/services/product.service";
 import Image from "next/image";
 import Link from "next/link";
@@ -69,31 +68,32 @@ export default function Page() {
 
   return (
     <ContainerLayout>
-      <div className="w-full flex gap-5 flex-wrap">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
         {data.products &&
-          Array.isArray(data.products) &&
           data.products.map((product) => (
             <Link key={product.id} href={`/product/${product.id}`}>
-              <div className="h-auto bg-zinc-700 border-2 border-white rounded-md min-w-80 p-3">
+              <div className="h-auto rounded-2xl p-5 border border-zinc-700 lg:p-4">
                 <Image
                   src={product.thumbnail}
                   alt={product.title}
                   width={100}
                   height={100}
-                  className="h-1/2  w-full rounded-sm "
+                  className="h-1/2 w-full rounded-sm bg-white lg:rounded-lg"
                 />
-                <div className="mt-2 flex flex-col gap-3">
+                <div className="mt-2.5 flex flex-col gap-1">
                   <div className="w-full flex justify-between">
-                    <p className="font-medium text-sm text-zinc-400">
+                    <p className="font-medium text-base text-zinc-400 lg:text-sm">
                       {product.category}
                     </p>
-                    <p className="font-medium text-sm text-yellow-300">
-                      {product.rating}
+                    <p className="font-medium text-base text-yellow-300 lg:text-sm">
+                      ⭐ {product.rating}
                     </p>
                   </div>
-                  <h3 className="font-semibold text-xl">{product.title}</h3>
-                  <h3 className="font-semibold text-xl text-blue-700">
-                    {product.price}
+                  <h3 className="font-semibold text-lg lg:text-base">
+                    {product.title}
+                  </h3>
+                  <h3 className="font-medium text-xl text-blue-700 lg:text-base text-right mt-1">
+                    $ {product.price}
                   </h3>
                 </div>
               </div>
--- layouts/container-layout.tsx
+++ layouts/container-layout.tsx
@@ -8,7 +8,7 @@ export default function ContainerLayout({ children }: ContainerLayout) {
   return (
     <>
       <Header />
-      <section className="min-h-screen pt-28 px-5 md:px-10 mx-auto lg:px-24 xl:px-36 2xl:px-40">
+      <section className="min-h-screen pt-28 px-5 md:px-10 lg:px-24 xl:px-40 2xl:px-64 pb-32 lg:pt-32">
         {children}
       </section>
     </>
