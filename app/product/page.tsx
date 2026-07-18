35a2d4ad971c9ad15fa13bf12a58e35ab46862fc
FajarVerse <gifarifajarm174@gmail.com>
2026-07-18 10:05:10 +0700

    feat: create card product component

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import type { Auth } from "@/app/login/page";
+import CardProduct from "@/components/card-product";
 import ContainerLayout from "@/layouts/container-layout";
 import { getProduct } from "@/services/product.service";
-import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -33,11 +32,6 @@ export default function Page() {
     skip: 0,
     limit: 0,
   });
-
-  const onLogout = () => {
-    localStorage.removeItem("account-user");
-    router.replace("/login");
-  };
 
   useEffect(() => {
     const getData = async () => {
@@ -71,33 +65,18 @@ export default function Page() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
         {data.products &&
           data.products.map((product) => (
-            <Link key={product.id} href={`/product/${product.id}`}>
-              <div className="h-auto rounded-2xl p-5 border border-zinc-700 lg:p-4">
-                <Image
-                  src={product.thumbnail}
-                  alt={product.title}
-                  width={100}
-                  height={100}
-                  className="h-1/2 w-full rounded-sm bg-white lg:rounded-lg"
-                />
-                <div className="mt-2.5 flex flex-col gap-1">
-                  <div className="w-full flex justify-between">
-                    <p className="font-medium text-base text-zinc-400 lg:text-sm">
-                      {product.category}
-                    </p>
-                    <p className="font-medium text-base text-yellow-300 lg:text-sm">
-                      ⭐ {product.rating}
-                    </p>
-                  </div>
-                  <h3 className="font-semibold text-lg lg:text-base">
-                    {product.title}
-                  </h3>
-                  <h3 className="font-medium text-xl text-blue-700 lg:text-base text-right mt-1">
-                    $ {product.price}
-                  </h3>
-                </div>
-              </div>
-            </Link>
+            <CardProduct key={product.id} id={product.id}>
+              <CardProduct.Header
+                imgUrl={product.thumbnail}
+                title={product.title}
+              />
+              <CardProduct.Body
+                title={product.title}
+                category={product.category}
+                rating={product.rating}
+              />
+              <CardProduct.Footer price={product.price} />
+            </CardProduct>
           ))}
       </div>
     </ContainerLayout>
---
+++ components/card-product.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface CardProductProps {
+  children: React.ReactNode;
+  id: number;
+}
+
+interface CardHeaderProps extends Pick<CardBodyProps, "title"> {
+  imgUrl: string;
+}
+
+interface CardBodyProps {
+  title: string;
+  category: string;
+  rating: number;
+}
+
+interface CardFooterProps {
+  price: number;
+}
+
+export default function CardProduct({ children, id }: CardProductProps) {
+  return (
+    <Link key={id} href={`/product/${id}`}>
+      <div className="h-auto rounded-2xl p-5 border border-zinc-700 lg:p-4">
+        {children}
+      </div>
+    </Link>
+  );
+}
+
+function CardHeader({ imgUrl, title }: CardHeaderProps) {
+  return (
+    <Image
+      src={imgUrl}
+      alt={title}
+      width={100}
+      height={100}
+      className="h-1/2 w-full rounded-sm bg-white lg:rounded-lg"
+    />
+  );
+}
+
+function CardBody({ title, category, rating }: CardBodyProps) {
+  return (
+    <div className="mt-2.5 flex flex-col gap-1">
+      <div className="w-full flex justify-between">
+        <p className="font-medium text-base text-zinc-400 lg:text-sm">
+          {category}
+        </p>
+        <p className="font-medium text-base text-yellow-300 lg:text-sm">
+          ⭐ {rating}
+        </p>
+      </div>
+      <h3 className="font-semibold text-lg lg:text-base">{title}</h3>
+    </div>
+  );
+}
+
+function CardFooter({ price }: CardFooterProps) {
+  return (
+    <p className="font-medium text-xl text-blue-700 lg:text-base text-right mt-1">
+      $ {price}
+    </p>
+  );
+}
+
+CardProduct.Header = CardHeader;
+CardProduct.Body = CardBody;
+CardProduct.Footer = CardFooter;
